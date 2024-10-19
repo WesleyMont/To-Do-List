c76734fd777204c1e63aea19ace5ce3e044b3f1d
Wesley Monteiro <89531052+WesleyMont@users.noreply.github.com>
2024-10-19 18:26:55 -0300

    Atualização do front Remoção de API para melhor integração

--- API/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql"
-
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password:"Wesley12345678.",
-    database: "crud",
-});
--- API/routes/lista.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {getLista} from "../controllers/lista.js"
-
-const router = express.Router()
-
-router.get("/", getLista)
-
-export default router
