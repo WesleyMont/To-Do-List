b5139655b31776459d09c0846d75bdcda0f3c74f
Wesley Monteiro <89531052+WesleyMont@users.noreply.github.com>
2024-10-22 22:17:10 -0300

    Atualizações para o db

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+<?php
+require_once('./db/conn.php');
+
+$tasks= [];
+
+$sql = $pdo->query("SELECT * FROM tasks");
+
+if ($sql->rowCount() > 0) {
+  $tasks = $sql->fetchALL(PDO::FETCH_ASSOC);
+  var_dump($tasks);
+}
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,12 +17,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Tarefas</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
 <body> 
     <div id="to_do">
         <h1> Lista de Tarefas </h1>
       <form action="" class="to-do-form">
-        <input type="text" name="description" placeholder="Começe sua lista aqui" required>     
+        <input 
+        type="text" 
+        name="description" 
+        placeholder="Começe sua lista aqui" 
+
+        required>     
 
         <button type="submit" class="form-button"> 
             
@@ -20,15 +38,22 @@
       </form>
 
       <div id="tasks">
+        <?php foreach($tasks as $task): ?>
         <div class="task">
-            <input type="checkbox" name="progress" class="progress">
+            <input 
+            type="checkbox" 
+            name="progress" 
+            class="progress"
+            <?= $task ['completed'] ? 'checked' : '' ?>
+            >
 
             <p class="task-description">
-                teste
+              <?= $task['description'] ?>
+                
             </p>
 
             <div class="task-actions">
-                <a href="" class="action-button edit-button">
+                <a class="action-button edit-button">
                     <i class="fa-solid fa-pen-to-square"></i>
                     </a>
                     <a href="" class="action-button delet-button">
@@ -43,8 +68,9 @@
             </button>
             </form>
         </div>
+        <?php endforeach ?>
       </div>
     </div>
-    
+    <script src="./javascript/script.js"></script>
 </body>
 </html>
